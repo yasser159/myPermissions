7b46ff416174ee03828ae10eb244263a8cdd553d
Yasser Hajlaoui <yasser159@gmail.com>
2022-05-08 22:05:46 -0700

    Initial Commit

---
+++ myPermissions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myPermissions
+//
+//  Created by Yasser Hajlaoui on 5/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
